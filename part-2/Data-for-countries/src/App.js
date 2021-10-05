c0d37fd1517c93dd8a05159d85bb7c81f4b0e05b
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-05 23:16:25 +0400

    Added single country descriptions

--- part-2/Data-for-countries/src/App.js
+++ part-2/Data-for-countries/src/App.js
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Display = ({ arr, input }) => {
+const SingleDescription = ({ arr }) => {
+	return(
+		<div>
+			<h2>{arr.name.common}</h2>
+			<p>Capital: {arr.capital}</p>
+			<p>Population: {arr.population}</p>
+			<h2>Languages</h2>
+			<ul>
+				{Object.entries(arr.languages).map(value => <li key={value[0]}>{value[1]}</li>)}
+			</ul>
+			<img src={arr.flags.png} width={120} alt="Flag"></img>
+		</div>
+	);
+}
+
+const HandleFilter = ({ arr, input }) => {
 	let res = [];
 	for(const i of arr) {
 		if(arr.length > 10) {
-			return <p>Too many matches, specify another filter</p>
+			return "\nToo many matches, specify another filter"
 		} else if(arr.length <= 10 && arr.length > 1) {
-			res.push(<p key={i.altSpellings[0]}>{i.name.common}</p>);
-		} 
-		else if(input.length === 0) return null;
+			res.push(i);
+		} else if(input.length === 0) return null;
 	}
-	return res;
-}
 
+	for(const j of arr) {
+		if(input === j.name.common) {
+			console.log(j.name.common, j);
+			return <SingleDescription arr={j} />		
+		}
+	}
+
+	return res.map(value => <p key={value.altSpellings[0]}>{value.name.common}</p>)
+}
 
 const App = () => {
 	const [countries, setCountries] = useState([]);
@@ -35,7 +56,7 @@ const App = () => {
 	return (
 		<div>
 			Find countries <input onChange={handleInput} />
-			<Display arr={result} input={input} />
+			<HandleFilter arr={result} input={input} />
 		</div>
 	);
 }
