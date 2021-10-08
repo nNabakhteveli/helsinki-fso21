540dad6728074c9887f7f3c1e5178525323bba36
nNabakhteveli <nabakhtevelin@gmail.com>
2021-10-08 04:43:28 +0400

    Added weather info in single descriptions

--- part-2/Data-for-countries/src/App.js
+++ part-2/Data-for-countries/src/App.js
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SingleDescription = ({ arr }) => {
+	const [weatherResponse, setWeatherResponse] = useState({});
+	const [isloaded, setLoaded] = useState(false);
+	
+	useEffect(() => {
+		const APIKey = process.env.REACT_APP_API_KEY;
+
+		axios.get(`http://api.weatherstack.com/current?access_key=${APIKey}&query=${arr.capital}`).then(response => {
+			setLoaded(false);
+			setWeatherResponse(response.data);
+			setLoaded(true);
+		})
+	}, [arr]);
+	
 	return(
 		<div>
 			<h2>{arr.name.common}</h2>
@@ -12,24 +25,38 @@ const SingleDescription = ({ arr }) => {
 				{Object.entries(arr.languages).map(value => <li key={value[0]}>{value[1]}</li>)}
 			</ul>
 			<img src={arr.flags.png} width={120} alt="Flag" />
+
+			<h2>Weather in {arr.capital}</h2>
+			<p>Temperature: {isloaded ? weatherResponse.current.temperature : "Wait to load the data"} Celsius</p>
+			<img src={isloaded ? weatherResponse.current.weather_icons[0] : ""} width={90} alt="Weather icon" />
+			{isloaded ? 
+				<p>Wind: {weatherResponse.current.wind_speed} mph, Direction - {weatherResponse.current.wind_dir}</p>
+				: 
+				<p>Wait to load the data</p>
+			}
 		</div>
 	);
 }
 
 const HandleFilter = ({ arr, input }) => {
-	console.log(arr);
-	let res = [];
+	let res = [], btn = [];
+
 	for(const i of arr) {
 		if(arr.length > 10) {
 			return <p>Too many matches, specify another filter</p>
 		} else if(input.length === 0) return null;
-		res.push(<p key={i.altSpellings[0]}>{i.name.common}</p>);
+
+		res.push(
+			<div key={i.altSpellings[0]}>
+				<p>{i.name.common}</p>
+				<button onClick={() => btn.push(i)}>show</button>
+			</div>
+		);
 	}
 
-	for(const j of arr) {
+	for(const j of arr) 
 		if(input === j.name.common || arr.length === 1) return <SingleDescription arr={j} />		
-	}
-
+	
 	return res;
 }
 
@@ -37,13 +64,12 @@ const App = () => {
 	const [countries, setCountries] = useState([]);
 	const [input, setInput] = useState("");
 	const [result, setResult] = useState([]);
-
+	
 	useEffect(() => {
 		axios.get('https://restcountries.com/v3.1/all').then(response => {
 			setCountries(response.data); 
 		})
 	}, []);
-
 
 	const handleInput = (event) => {
 		setInput(event.target.value);
