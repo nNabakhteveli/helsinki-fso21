72525a9d0874ee70b83a9c3b3e4898ee3e20ee49
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2021-12-29 22:52:13 +0400

    Added react styled components to the anecdotes app

--- part-7/7.1-7.3-anecdotes/src/App.js
+++ part-7/7.1-7.3-anecdotes/src/App.js
@@ -4,6 +4,37 @@ import { getId } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 
 const CreateAnecdote = ({ addingHandler }) => {
@@ -11,9 +42,9 @@ const CreateAnecdote = ({ addingHandler }) => {
     <form onSubmit={addingHandler}>
       <h2>create new</h2>
       <div>
-        <input type="text" name="anecdoteInput" />
+        <Input type="text" name="anecdoteInput" />
       </div>
-      <Link to="/"><button type='submit'>create</button></Link>
+      <a href='http://localhost:3000/'><Button type='submit'>create</Button></a>
     </form>
   );
 }
@@ -21,11 +52,11 @@ const CreateAnecdote = ({ addingHandler }) => {
 const Navbar = () => {
   const style = { margin: '5px' }
   return(
-    <nav>
+    <Navigation>
       <Link to="/" style={style}>Home</Link>
       <Link to="/anecdotes" style={style}>Anecdotes</Link>
       <Link to="/new-anecdote" style={style}>Create new</Link>
-    </nav>
+    </Navigation>
   );
 }
 
@@ -69,16 +100,17 @@ const App = () => {
     }
   };
 
-  const addAnecdote = event => {
+  const addAnecdote = async event => {
     event.preventDefault();
 
+    console.log("I got executed blin")
     const newAnecdoteData = {
       content: event.target.anecdoteInput.value,
       id: getId(),
       votes: 0
     }
 
-    axios.post('http://localhost:3001/anecdotes', newAnecdoteData);
+    await axios.post('http://localhost:3001/anecdotes', newAnecdoteData);
     
     dispatch({ 
       type: "ADD_ANECDOTE", 
@@ -87,43 +119,47 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <h1>Software anecdotes</h1>
-      <Navbar />
-      <div>
-        <Notification />
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <h1>Anecdotes</h1>
-              <ul>
-                {anecdotes.map((value, key) => <li key={key}><Link to={`/anecdotes/${value.id}`}>{value.content}</Link></li>)}
-              </ul>
-            </div>
-          } />
-          <Route path="/anecdotes" element={
-            <div>
-              <h2>Anecdotes</h2>
-              <b>Filter</b> <input type="text" name="filterInput" onChange={handleFilter} />
-              {filterData.map(anecdote =>
-                <div key={anecdote.id}>
-                  <div>
-                    {anecdote.content}
+    <Page>
+      <Router>
+        <div className='container'>
+        <Navbar />
+        <h1>Software anecdotes</h1>
+          <Notification />
+          <Routes>
+            <Route path="/" element={
+              <div>
+                <h1>Anecdotes</h1>
+                <ul>
+                  {anecdotes.map((value, key) => <li key={key}><Link to={`/anecdotes/${value.id}`}>{value.content}</Link></li>)}
+                </ul>
+              </div>
+            } />
+            <Route path="/anecdotes" element={
+              <div>
+                <h2>Anecdotes</h2>
+                <b>Filter</b> <input type="text" name="filterInput" onChange={handleFilter} />
+                {filterData.map(anecdote =>
+                  <div key={anecdote.id}>
+                    <div>
+                      {anecdote.content}
+                    </div>
+                    <div>
+                      has {anecdote.votes}
+                      <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
                   </div>
-                  <div>
-                    has {anecdote.votes}
-                    <button onClick={() => vote(anecdote.id)}>vote</button>
-                  </div>
-                </div>
-              )}
-            </div>
-          } />
-        <Route path='/new-anecdote' element={<CreateAnecdote addingHandler={e => addAnecdote} />} />
-        
-        </Routes>
-        <p>Anecdote app for Full Stack Open. See <a href="https://github.com/nNabakhteveli/helsinki-fso21/tree/main/part-6/" target='_blank'>https://github.com/nNabakhteveli/helsinki-fso21/tree/main/part-6/</a>  for the source code.</p>
-      </div>
-    </Router>
+                )}
+              </div>
+            } />
+          <Route path='/new-anecdote' element={<CreateAnecdote addingHandler={addAnecdote} />} />
+          
+          </Routes>
+          <Footer>
+            <p>Anecdote app for Full Stack Open. See <a href="https://github.com/nNabakhteveli//helsinki-fso21/tree/main/part-6/" target='_blank'>https://github.com/nNabakhteveli/helsinki-fso21/tree/main/part-6/</a>  for the source code.</p>
+          </Footer>
+        </div>
+      </Router>
+    </Page>
   )
 }
 
